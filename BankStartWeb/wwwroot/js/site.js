0fbdd2ac1d2fc6df7460e87070d3700b00928ea8
Borangs2 <andbor02@gmail.com>
2022-03-24 16:32:19 +0100

    Added template

--- BankStartWeb/wwwroot/js/site.js
+++ BankStartWeb/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(function () {
+    $(".navbar-toggler").on("click", function (e) {
+        $(".tm-header").toggleClass("show");
+        e.stopPropagation();
+    });
+
+    $("html").click(function (e) {
+        var header = document.getElementById("tm-header");
+
+        if (!header.contains(e.target)) {
+            $(".tm-header").removeClass("show");
+        }
+    });
+
+    $("#tm-nav .nav-link").click(function (e) {
+        $(".tm-header").removeClass("show");
+    });
+});
