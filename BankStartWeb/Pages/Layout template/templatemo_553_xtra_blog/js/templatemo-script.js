62ebbd42a5d16b382594e06958439f6a7c66f7b9
Borangs2 <andbor02@gmail.com>
2022-03-29 10:06:49 +0200

    Revert "Fixed layout and added back icons"
    This reverts commit 3ef6abf30e566320e2388a6ac5cec6b0e6278fd6.

--- BankStartWeb/Pages/Layout template/templatemo_553_xtra_blog/js/templatemo-script.js
+++ BankStartWeb/Pages/Layout template/templatemo_553_xtra_blog/js/templatemo-script.js
@@ -1,31 +1,18 @@
-var navbarBtn = document.getElementById("navbar-toggler");
-console.log(navbarBtn);
-
-navbarBtn.addEventListener("Click", () => {
-
-
-    alert("Clicked")
-
-
-
-
-
-
-
-    //$(".navbar-toggler").on("click", function (e) {
-    //    $(".tm-header").toggleClass("show");
-    //    e.stopPropagation();
-    //});
-
-    //$("html").click(function (e) {
-    //    var header = document.getElementById("tm-header");
-
-    //    if (!header.contains(e.target)) {
-    //        $(".tm-header").removeClass("show");
-    //    }
-    //});
-
-    //$("#tm-nav .nav-link").click(function (e) {
-    //    $(".tm-header").removeClass("show");
-    //});
+$(function() {
+    $(".navbar-toggler").on("click", function(e) {
+        $(".tm-header").toggleClass("show");
+        e.stopPropagation();
+      });
+    
+      $("html").click(function(e) {
+        var header = document.getElementById("tm-header");
+    
+        if (!header.contains(e.target)) {
+          $(".tm-header").removeClass("show");
+        }
+      });
+    
+      $("#tm-nav .nav-link").click(function(e) {
+        $(".tm-header").removeClass("show");
+      });
 });
--- BankStartWeb/wwwroot/js/site.js
+++ BankStartWeb/wwwroot/js/site.js
@@ -4,51 +4,21 @@
 // Write your JavaScript code.
 
 
-var navbarBtn = document.getElementById("navbar-toggler");
-var header = document.getElementById("tm-header");
+$(function () {
+    $(".navbar-toggler").on("click", function (e) {
+        $(".tm-header").toggleClass("show");
+        e.stopPropagation();
+    });
 
+    $("html").click(function (e) {
+        var header = document.getElementById("tm-header");
 
-navbarBtn.addEventListener("click", () => {
-    header.classList.toggle("show");
+        if (!header.contains(e.target)) {
+            $(".tm-header").removeClass("show");
+        }
+    });
 
-    //$(".navbar-toggler").on("click", function (e) {
-    //    $(".tm-header").toggleClass("show");
-    //    e.stopPropagation();
-    //});
-
-    //$("html").click(function (e) {
-    //    var header = document.getElementById("tm-header");
-
-    //    if (!header.contains(e.target)) {
-    //        $(".tm-header").removeClass("show");
-    //    }
-    //});
-
-    //$("#tm-nav .nav-link").click(function (e) {
-    //    $(".tm-header").removeClass("show");
-    //});
+    $("#tm-nav .nav-link").click(function (e) {
+        $(".tm-header").removeClass("show");
+    });
 });
-
-
-
-var url = window.location.pathname;
-var filename = url.split('/').pop();
-
-var homeBtn = document.getElementById("homeBtn");
-var custBtn = document.getElementById("custBtn");
-
-switch (filename) {
-    //Index
-    case "":
-        homeBtn.classList.add("active");
-        break;
-    case "Customers":
-        custBtn.classList.add("active");
-}
-
-
-
-function RemoveActiveButton() {
-    homeBtn.classList.remove("active");
-    custBtn.classList.remove("active");
-}
