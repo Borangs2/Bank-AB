b3f63c35418f8c18d9ad031c52599405ad5d1766
Borangs2 <andbor02@gmail.com>
2022-03-29 10:15:41 +0200

    Fixed layout and added back icons and responsiveness again, becasue im dumb and reverted it all

--- BankStartWeb/wwwroot/js/site.js
+++ BankStartWeb/wwwroot/js/site.js
@@ -1,24 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿var navbarBtn = document.getElementById("navbar-toggler");
+var header = document.getElementById("tm-header");
 
 
-$(function () {
-    $(".navbar-toggler").on("click", function (e) {
-        $(".tm-header").toggleClass("show");
-        e.stopPropagation();
-    });
-
-    $("html").click(function (e) {
-        var header = document.getElementById("tm-header");
-
-        if (!header.contains(e.target)) {
-            $(".tm-header").removeClass("show");
-        }
-    });
-
-    $("#tm-nav .nav-link").click(function (e) {
-        $(".tm-header").removeClass("show");
-    });
+navbarBtn.addEventListener("click", () => {
+    header.classList.toggle("show");
 });
+
+
+
+var url = window.location.pathname;
+var filename = url.split('/').pop();
+
+var homeBtn = document.getElementById("homeBtn");
+var custBtn = document.getElementById("custBtn");
+
+switch (filename) {
+    //Index
+    case "":
+        homeBtn.classList.add("active");
+        break;
+    case "Customers":
+        custBtn.classList.add("active");
+}
