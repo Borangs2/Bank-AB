f63d561175db2700c1dfef0fff74d6f2189c2adc
Borangs2 <andbor02@gmail.com>
2022-04-12 12:42:06 +0200

    Added transactionservice

---
+++ BankStartWeb/Services/ITransactionsService.cs
@@ -0,0 +1,21 @@
+﻿namespace Bank_AB.Services
+{
+    public interface ITransactionsService
+    {
+        public enum ReturnCode
+        {
+            Ok,
+            ValueNegative,
+            BalanceToLow,
+            NotFound,
+        }
+
+        ReturnCode Withdraw(int accountId, decimal amount);
+        ReturnCode Deposit(int accountId, decimal amount);
+        ReturnCode Transfer(int fromAccountId, int toAccountId, decimal amount);
+
+
+
+
+    }
+}
---
+++ BankStartWeb/Services/TransactionsService.cs
@@ -0,0 +1,20 @@
+﻿namespace Bank_AB.Services
+{
+    public class TransactionsService : ITransactionsService
+    {
+        public ITransactionsService.ReturnCode Deposit(int accountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITransactionsService.ReturnCode Transfer(int fromAccountId, int toAccountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITransactionsService.ReturnCode Withdraw(int accountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
