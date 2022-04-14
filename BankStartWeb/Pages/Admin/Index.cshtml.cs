4cdbd82e13baa33bb9fe2b57156766bf84b3a184
Borangs2 <andbor02@gmail.com>
2022-04-14 10:55:52 +0200

    Started with admin CRU

--- BankStartWeb/Pages/Admin/Index.cshtml.cs
+++ BankStartWeb/Pages/Admin/Index.cshtml.cs
@@ -2,14 +2,17 @@ using BankStartWeb.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bank_AB.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        public UserManager<IdentityUser> _userManager { get; }
         public ApplicationDbContext _context { get; }
-        public IndexModel(ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
@@ -27,6 +30,10 @@ namespace Bank_AB.Pages.Admin
             AllUsers = _context.Users.ToList();
         }
 
-
+        public async Task<string[]> GetUserRoles(string id)
+        {
+            var roles = await _userManager.GetRolesAsync(_context.Users.First(u => u.Id == id));
+            return roles.ToArray();
+        }
     }
 }
---
+++ BankStartWeb/Pages/Admin/ViewUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bank_AB.Pages.Admin
+{
+    public class ViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
