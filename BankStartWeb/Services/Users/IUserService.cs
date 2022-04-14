f7493ecb5d6cb51802c74eaa889ae6d6bf9965cf
Borangs2 <andbor02@gmail.com>
2022-04-14 14:08:48 +0200

    Added userservices

---
+++ BankStartWeb/Services/Users/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Bank_AB.Services.Users
+{
+    public interface IUserService
+    {
+        public enum ReturnCode
+        {
+            Ok,
+            NotFound,
+        }
+        IdentityUser? GetUserById(string userId);
+        ReturnCode CreateUser(IdentityUser updatedUser);
+        ReturnCode UpdateUser(IdentityUser updatedUser);
+
+
+
+
+    }
+}
---
+++ BankStartWeb/Services/Users/UserService.cs
@@ -0,0 +1,37 @@
+﻿using BankStartWeb.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bank_AB.Services.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserService(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public IdentityUser? GetUserById(string userId)
+        {
+            return _context.Users.FirstOrDefault(u => u.Id == userId); 
+        }
+
+        public IUserService.ReturnCode CreateUser(IdentityUser newUser)
+        {
+            _userManager.CreateAsync(newUser);
+
+            return IUserService.ReturnCode.Ok;
+        }
+
+
+
+        public IUserService.ReturnCode UpdateUser(IdentityUser updatedUser)
+        {
+            _userManager.UpdateAsync(updatedUser);
+
+            return IUserService.ReturnCode.Ok;
+        }
+    }
+}
