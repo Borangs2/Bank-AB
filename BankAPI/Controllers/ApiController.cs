35071a2502e5f8a081236124eaf8f65a22467a07
Borangs2 <andbor02@gmail.com>
2022-05-05 09:55:58 +0200

    Started on API without conficts this time

---
+++ BankAPI/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bank_AB.Services.Customers;
+using Bank_AB.Services.Accounts;
+using BankAPI.ViewModels;
+
+namespace BankAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class APIController : Controller
+    {
+        private readonly CustomerService _customerService;
+        private readonly AccountService _accountService;
+
+        public APIController(CustomerService customerService, AccountService accountService)
+        {
+            _customerService = customerService;
+            _accountService = accountService;
+        }
+
+        
+
+
+
+
+    }
+}
