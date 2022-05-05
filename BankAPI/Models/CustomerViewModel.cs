e7bdfd9d6a6e86bf12960af6565d3cfd7f7f093e
Borangs2 <andbor02@gmail.com>
2022-05-05 09:16:38 +0200

    Started on API

---
+++ BankAPI/Models/CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BankAPI.Models
+{
+    public class CustomerViewModel
+    {
+        public int Id { get; set; }
+        public string Givenname { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string Telephone { get; set; } = null!;
+        public string EmailAddress { get; set; } = null!;
+        public string City { get; set; } = null!;
+
+
+    }
+}
